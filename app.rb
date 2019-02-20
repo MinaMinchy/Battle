52cb1584f570881e20976cfaa5b87bb2e7c1bc8a
Amina Jusic <amina_jusic@yahoo.com>
2019-02-20 15:35:31 +0000

    Enable session POST-redirect loop

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
     get'/' do
       erb :index
     end
 
 post '/names' do
-  @player_1_name = params[:player_1_name]
-  @player_2_name = params[:player_2_name]
-  erb :play
+  # session["init"] = true
+p  session[:player_1_name] = params[:player_1_name]
+p  session[:player_2_name]= params[:player_2_name]
+  redirect to('/play')
+end
+
+get '/play' do
+p  @player_1_name = session[:player_1_name]
+p  @player_2_name = session[:player_2_name]
+  erb(:play)
 end
 
 
